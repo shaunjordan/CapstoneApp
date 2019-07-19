e5345a628ba2b095a0b1450fb531e1ba645b07b4
Shaun Jordan <hello@shaunmjordan.com>
2019-07-19 14:19:10 -0500

    init

---
+++ CapstoneApp/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CapstoneApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneApp.ViewModels
+{
+    public class CustomerListViewModel
+    {
+        //getcustomers gc = new GetCustomers();
+
+        public CustomerListViewModel()
+        {
+            //Customers = gc.GetCustomersAsync()
+        }
+
+        public ObservableCollection<Customer> Customers { get; set; }
+    }
+}
